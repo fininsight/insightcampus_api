29c1dafc921f2796bf68741d9649c9d042a769c6
mona <kjwook7522@gmail.com>
2020-06-25 00:41:32 +0900

    강의메뉴 공지 API 완성

---
+++ insightcampus_api/Dao/ClassNoticeInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using insightcampus_api.Data;
+using insightcampus_api.Model;
+
+namespace insightcampus_api.Dao
+{
+    public interface ClassNoticeInterface
+    {
+        Task Add<T>(T entity) where T : class;
+        Task Delete<T>(T entity) where T : class;
+        Task Update(ClassNoticeModel classNoticeModel);
+        Task<DataTableOutDto> Select(DataTableInputDto dataTableInputDto);
+        Task<ClassNoticeModel> Select(int class_notice_seq);
+    }
+}
