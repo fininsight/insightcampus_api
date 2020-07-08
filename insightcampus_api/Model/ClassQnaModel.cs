d662897dab2b3b5a03c082ed6f9c4a53597d7a31
mona <kjwook7522@gmail.com>
2020-07-08 15:13:53 +0900

    QnA API 완성

---
+++ insightcampus_api/Model/ClassQnaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace insightcampus_api.Model
+{
+    [Table("class_qna")]
+    public class ClassQnaModel
+    {
+        [Key]
+        public int class_qna_seq { get; set; }
+        public int class_seq { get; set; }
+        public int parent_seq { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public int reg_user { get; set; }
+        public DateTime reg_dt { get; set; }
+        public int upd_user { get; set; }
+        public DateTime upd_dt { get; set; }
+    }
+}
