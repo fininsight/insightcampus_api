bed5db1d3770bfdded9382c0cbaa43261961497d
kjwook7522 <kjwook7522@gmail.com>
2020-07-22 23:35:12 +0900

    selected incamfare pdf 출력 완료

---
+++ insightcampus_api/Dao/PdfRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using insightcampus_api.Data;
+using insightcampus_api.Model;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace insightcampus_api.Dao
+{
+    public class PdfRepository : PdfInterface
+    {
+        private readonly DataContext _context;
+
+        public PdfRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IncamAddfareModel> Select(int addfare_seq)
+        {
+            var result = (
+                    from incam_addfare in _context.PdfContext
+                    where incam_addfare.addfare_seq == addfare_seq
+                    select incam_addfare).SingleAsync();
+
+
+            return result;
+        }
+
+    }
+}
