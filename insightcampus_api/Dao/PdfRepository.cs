d44d8c0d4ba9b735e301e40c6c9b6de8ebd5347b
kjwook7522 <kjwook7522@gmail.com>
2020-09-08 02:15:14 +0900

    pdf 테이블 수정

--- insightcampus_api/Dao/PdfRepository.cs
+++ insightcampus_api/Dao/PdfRepository.cs
@@ -19,9 +19,25 @@ namespace insightcampus_api.Dao
         public Task<IncamAddfareModel> Select(int addfare_seq)
         {
             var result = (
-                    from incam_addfare in _context.PdfContext
+                    from incam_addfare in _context.IncamAddfareContext
+                    join incam_contract in _context.IncamContractContext on incam_addfare.contract_seq equals incam_contract.contract_seq
+                    join teacher in _context.TeacherContext on incam_contract.teacher_seq equals teacher.teacher_seq
                     where incam_addfare.addfare_seq == addfare_seq
-                    select incam_addfare).SingleAsync();
+                    select new IncamAddfareModel
+                    {
+                        addfare_seq = incam_addfare.addfare_seq,
+                        contract_seq = incam_addfare.contract_seq,
+                        hour = incam_addfare.hour,
+                        addfare_date = incam_addfare.addfare_date,
+                        income_type = incam_addfare.income_type,
+                        original_company_nm = incam_contract.original_company,
+                        @class = incam_contract.@class,
+                        name = teacher.name,
+                        hour_incen = incam_contract.hour_incen,
+                        hour_price = incam_contract.hour_price,
+                        contract_price = incam_contract.contract_price
+                    }).SingleAsync();
+                    //select incam_addfare).SingleAsync();
 
 
             return result;
