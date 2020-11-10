1239b660cc1897304a4ea2837e46f656b28c9d40
kjwook7522 <kjwook7522@gmail.com>
2020-11-10 18:15:35 +0900

    community API생성 및 기능 추가

---
+++ insightcampus_api/Controllers/CommunityController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using insightcampus_api.Dao;
+using insightcampus_api.Data;
+using insightcampus_api.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace insightcampus_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommunityController : ControllerBase
+    {
+        private readonly CommunityInterface  _community;
+
+        public CommunityController(CommunityInterface community)
+        {
+            _community = community;
+        }
+
+        [HttpGet("{size}/{pageNumber}")]
+        public async Task<ActionResult<DataTableOutDto>> Get(int size, int pageNumber)
+        {
+            DataTableInputDto dataTableInputDto = new DataTableInputDto();
+            dataTableInputDto.size = size;
+            dataTableInputDto.pageNumber = pageNumber;
+
+            return await _community.Select(dataTableInputDto);
+        }
+
+        [HttpGet("{board_seq}")]
+        public async Task<ActionResult<CommunityModel>> Get(int board_seq)
+        {
+            return await _community.Select(board_seq);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CommunityModel board)
+        {
+            board.reg_dt = DateTime.Now;
+            board.upd_dt = DateTime.Now;
+            await _community.Add(board);
+            return Ok();
+        }
+
+        [HttpPut("{seq}")]
+        public async Task<ActionResult> Put(int seq, [FromBody] CommunityModel board)
+        {
+            board.upd_dt = DateTime.Now;
+            board.board_seq = seq;
+            await _community.Update(board);
+            return Ok();
+        }
+
+        [HttpDelete("{seq}")]
+        public async Task<ActionResult> Delete(int seq)
+        {
+            CommunityModel board = new CommunityModel
+            {
+                board_seq = seq
+            };
+            await _community.Delete(board);
+            return Ok();
+        }
+    }
+}
---
+++ insightcampus_api/Dao/CommunityInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using insightcampus_api.Data;
+using insightcampus_api.Model;
+
+namespace insightcampus_api.Dao
+{
+    public interface CommunityInterface
+    {
+        Task Add<T>(T entity) where T : class;
+        Task Delete<T>(T entity) where T : class;
+        Task Update(CommunityModel communityModel);
+        Task<DataTableOutDto> Select(DataTableInputDto dataTableInputDto);
+        Task<CommunityModel> Select(int board_seq);
+    }
+}
