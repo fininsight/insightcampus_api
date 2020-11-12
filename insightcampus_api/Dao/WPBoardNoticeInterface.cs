2aadecaf8830d9a6df80906029e3a008328dd8d4
LeeJinbeom <jinbomang@gmail.com>
2020-11-12 11:08:30 +0900

    NoticInterface Add

---
+++ insightcampus_api/Dao/WPBoardNoticeInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using insightcampus_api.Data;
+
+namespace insightcampus_api.Dao
+{
+    public interface WPBoardNoticeInterface
+    {
+        Task<DataTableOutDto> Select(DataTableInputDto dataTableInputDto);
+        Task<DataTableOutDto> SelectLibrary(DataTableInputDto dataTableInputDto);
+        Task<DataTableOutDto> SelectReview(DataTableInputDto dataTableInputDto);
+    }
+}
