78f44f0255facc92c285dc32c6ff4623f8a4bf44
Changpulmu <16906@naver.com>
2020-11-19 17:48:12 +0900

    Education_Certification_PDF

--- insightcampus_api/Controllers/PdfController.cs
+++ insightcampus_api/Controllers/PdfController.cs
@@ -59,5 +59,39 @@ namespace insightcampus_api.Controllers
             var file = _converter.Convert(pdf);
             return File(file, "application/pdf", addfare.name + "님_지급명세서_" + addfare.addfare_date.ToString("yyyy-MM-dd") + ".pdf"); // for downloading as sample.pdf
         }
+
+         [HttpGet("{eduCertification_seq}")]
+        public async Task<IActionResult> CreateEduCertificationPDF(int eduCertification_seq)
+        {
+            var eduCertification = await _pdf.Select(eduCertification_seq);
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DPI = 300,
+                DocumentTitle = "",
+            };
+
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetEduCertificationHTMLString(eduCertification),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "edu_certification_style.css") },
+                HeaderSettings = { FontName = "NanumGothic", FontSize = 9, Right = "", Line = false },
+                FooterSettings = { FontName = "NanumGothic", FontSize = 9, Right = "", Line = false }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf", eduCertification.name + "님_교육수료증_" + eduCertification.date.ToString("yyyy-MM-dd") + ".pdf");
+        }
     }
 }
