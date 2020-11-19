d4d3147908998238f67fd741c5367b36f2988996
Park J <ParkJ@Parkui-MacBook-Pro.local>
2020-11-19 13:58:37 +0900

    pdf_employ_proof

--- insightcampus_api/Controllers/PdfController.cs
+++ insightcampus_api/Controllers/PdfController.cs
@@ -59,5 +59,41 @@ namespace insightcampus_api.Controllers
             var file = _converter.Convert(pdf);
             return File(file, "application/pdf", addfare.name + "님_지급명세서_" + addfare.addfare_date.ToString("yyyy-MM-dd") + ".pdf"); // for downloading as sample.pdf
         }
+
+        [HttpGet("{employ_proof}")]
+        public async Task<IActionResult> CreateProofPDF(int employ_proof)
+        {
+            // sudo apt-get install libgdiplus
+
+            var proof = await _pdf.Select(employ_proof);
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DPI = 300,
+                DocumentTitle = "",
+            };
+
+
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = TemplateGenerator.GetProofHTMLString(proof),
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "pdfstyle.css") },
+                HeaderSettings = { FontName = "NanumGothic", FontSize = 9, Right = "", Line = false },
+                FooterSettings = { FontName = "NanumGothic", FontSize = 9, Right = "", Line = false }
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf", proof.name + "님_재직증명서_" + proof.addfare_date.ToString("yyyy-MM-dd") + ".pdf"); // for downloading as sample.pdf
+        }
     }
 }
