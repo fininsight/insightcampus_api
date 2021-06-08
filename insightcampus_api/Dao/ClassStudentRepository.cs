1a6f7a05d5a17714b9e2954c038017738a993ec0
kjwook7522 <kjwook7522@gmail.com>
2021-06-08 13:53:47 +0900

    수료증 이메일 내용 변경 및 파일 이름 변경

--- insightcampus_api/Dao/ClassStudentRepository.cs
+++ insightcampus_api/Dao/ClassStudentRepository.cs
@@ -77,12 +77,36 @@ namespace insightcampus_api.Dao
                           address = order.address,
                       }).SingleAsync();
 
-            string title = $"[핀인사이트] {result.name} 님 {result.class_nm} 수료증";
+            string title = $"[인사이트 캠퍼스/ {result.class_nm}] 수료증 및 종강 설문조사 안내드립니다.";
+            
+            string content = @"
+                <p>안녕하세요,</p> 
+                <p>인사이트 캠퍼스입니다.</p>
+                <p>강의 수강하시느라 수고 많으셨습니다.</p>
+                <br />
+                <p>=========================</p>
+                <p>- 수료증 (PDF 파일 첨부)</p>
+                <br />
+                <p>-설문조사 참여</p>
+                <p>더 나은 교육을 위해 설문을 진행합니다.</p>
+                <p>설문해주신 분들께 할인쿠폰을 드립니다.</p>
+                <br />
+                <p>*설문 맨 하단에 쿠폰코드가 있습니다.</p>
+                <p>*할인쿠폰 종강 후 6개월 내 / 1회 / 타쿠폰과 중복 사용 가능.</p>
+                <br />
+                <p>설문하러가기>> <a href='https://forms.gle/1J4X1q9znzLkaUst9'>https://forms.gle/1J4X1q9znzLkaUst9</a></p>
+                <br />
+                <p>=========================</p>
+                <br />
+                <p>좋은 교육으로 또 뵙겠습니다.</p>
+                <p>문의 있으시면 언제든 연락바랍니다.</p>
+                <p>감사합니다.</p>";
 
-            string content = $"{result.name}님  {result.class_nm} 수료증 보내드립니다.";
+            string[] sender = { "edu@fins.ai" };
 
-            string[] test = { "bill@fininsight.co.kr" };
-            await _email.SendEmail(result.email, title, content, file_path, test);
+            string fileName = $"{result.class_nm}강의수료증({result.name}님)";
+
+            await _email.SendEmail(result.email, title, content, file_path, fileName, sender);
 
             EmailLogModel emailLog = new EmailLogModel
             {
